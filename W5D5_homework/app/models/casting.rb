96921d5f8b1bc16dd4692d2b88a21eaf2826646e
Manel-Os <oulds.manel@gmail.com>
2023-06-01 22:14:18 -0400

    Completed homework1

---
+++ W5D5_homework/app/models/casting.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: castings
+#
+#  id         :bigint           not null, primary key
+#  movie_id   :bigint           not null
+#  actor_id   :bigint           not null
+#  ord        :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Casting < ApplicationRecord
+  validates :ord, presence: true
+  
+  belongs_to :actor,
+    class_name: 'Actor',
+    foreign_key: :actor_id,
+    primary_key: :id
+
+
+  belongs_to :movie,
+    class_name: 'Movie',
+    foreign_key: :movie_id,
+    primary_key: :id
+end
